b71cde40b0b5d85ec1f9b99ce1678747cb05c514
Pavel <pavasilev1994@gmail.com>
2023-02-15 21:28:08 +0300

    new version of conftest for task 3.6

--- conftest.py
+++ conftest.py
@@ -6,30 +6,13 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru,en,es,fr")
-
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    if user_language == "ru":
-        options.add_experimental_option('prefs', {'intl.accept_languages': "ru"})
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "en":
-        options.add_experimental_option('prefs', {'intl.accept_languages': "en"})
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "es":
-        options.add_experimental_option('prefs', {'intl.accept_languages': "es"})
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    elif user_language == "fr":
-        options.add_experimental_option('prefs', {'intl.accept_languages': "fr"})
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome(options=options)
-    else:
-        raise pytest.UsageError("--language should ru/en/es")
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
